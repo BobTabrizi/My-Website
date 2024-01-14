1d67f3e36170c3e50c5a38a73d84c5d13684539c
BobTabrizi <btabrizi@ucsd.edu>
2024-01-13 18:17:57 -0800

    remove astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
-import compress from 'astro-compress';
 import icon from 'astro-icon';
 import tasks from './src/utils/tasks';
 
@@ -62,17 +61,9 @@ export default defineConfig({
 
     tasks(),
 
-    compress({
-      CSS: true,
-      HTML: {
-        removeAttributeQuotes: false,
-      },
-      Image: false,
-      JavaScript: true,
-      SVG: true,
-      Logger: 1,
-    }),
-  ],
+    image: {
+      service: squooshImageService(),
+    },
 
   markdown: {
     remarkPlugins: [readingTimeRemarkPlugin],
